70505f2ed993cdba366bf6d27073b465d888c711
Evgeniy Polyakov <zbr@ioremap.net>
2012-09-11 03:05:48 +0400

    Use grapejson instead of plain json to prevent conflicts

--- cmake/Modules/Findelliptics.cmake
+++ cmake/Modules/Findelliptics.cmake
@@ -1,6 +1,6 @@
-find_path(elliptics_INCLUDE_DIR elliptics/cppdef.h PATH ${ELLIPTICS_PREFIX}/include /usr/include)
-find_path(eblob_INCLUDE_DIR eblob/blob.h PATH ${EBLOB_PREFIX}/include /usr/include)
-find_path(cocaine_INCLUDE_DIR cocaine/binary.hpp PATH ${COCAINE_PREFIX}/include /usr/include)
+find_path(elliptics_INCLUDE_DIR elliptics/cppdef.h PATHS ${ELLIPTICS_PREFIX}/include /usr/include)
+find_path(eblob_INCLUDE_DIR eblob/blob.h PATHS ${EBLOB_PREFIX}/include /usr/include)
+find_path(cocaine_INCLUDE_DIR cocaine/binary.hpp PATHS ${COCAINE_PREFIX}/include /usr/include)
 
 find_library(elliptics_LIBRARY NAMES elliptics PATHS ${ELLIPTICS_PREFIX}/lib ${ELLIPTICS_PREFIX}/lib64 /usr/lib /usr/lib64)
 find_library(elliptics_cpp_LIBRARY NAMES elliptics_cpp PATHS ${ELLIPTICS_PREFIX}/lib ${ELLIPTICS_PREFIX}/lib64 /usr/lib /usr/lib64)
@@ -8,6 +8,9 @@ find_library(eblob_LIBRARY NAMES eblob PATHS ${EBLOB_PREFIX}/lib ${EBLOB_PREFIX}
 
 set(elliptics_LIBRARIES ${elliptics_LIBRARY} ${elliptics_cpp_LIBRARY} ${eblob_LIBRARY})
 set(elliptics_INCLUDE_DIRS ${elliptics_INCLUDE_DIR} ${eblob_INCLUDE_DIR} ${cocaine_INCLUDE_DIR})
+
+message("elliptics_INCLUDE_DIR: " ${elliptics_INCLUDE_DIR})
+message("elliptics_INCLUDE_DIRS: " ${elliptics_INCLUDE_DIRS})
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set LIBXML2_FOUND to TRUE
